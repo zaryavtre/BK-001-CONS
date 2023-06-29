ae703124190523be1aa32f1adf1bbaed9aba9d80
Stéphane <von.zaryavtre@gmail.com>
2023-06-29 18:45:36 +0100

    update log 007

--- app.js
+++ app.js
@@ -1,51 +1,63 @@
-import { addFormDom, addBookBtn } from "./bookData.js"
+import { addFormDom, addBookBtn } from './bookData.js'
 let bookIndex = 0
 let myLibrary = []
 document.addEventListener('DOMContentLoaded', addBookBtn, addFormDom())
-document.addEventListener('click', function(e) {
-    if(e.target.matches('.add-btn')) {
-        document.querySelector('.form-overlay').classList.toggle('show-overlay')
-    } if(e.target.dataset.overlay){
-        document.querySelector('.form-overlay').classList.remove('.show-overlay')
-    }
+document.addEventListener('click', function (e) {
+  e.preventDefault()
+  if (e.target.matches('.add-btn')) {
+    document.querySelector('.form-overlay').classList.toggle('show-overlay')
+  }
+  if (e.target.dataset.overlay) {
+    document.querySelector('.form-overlay').classList.remove('.show-overlay')
+  }
+  if (e.target.matches('.sub-btn')) {
+    inputBook()
+  }
 })
 
-
 function Book(bookName, author, numberOfPages) {
-    this.bookName = bookName
-    this.author = author
-    this.numberOfPages = numberOfPages
-    this.index = bookIndex++
-
+  this.bookName = bookName
+  this.author = author
+  this.numberOfPages = numberOfPages
+  this.index = bookIndex++
 }
 
 const newBook = new Book('Game', 'Olissip', 124)
 const secBook = new Book('Mount Doom', 'Fior', 545)
 
-myLibrary.push(newBook, secBook) 
+myLibrary.push(newBook, secBook)
+
+function inputBook() {
+  const formBookName = document.querySelector('#book-name').value
+  const formBookAuthor = document.querySelector('#book-author').value
+  const formBookPages = document.querySelector('#book-pages').value
+  const newBooks = new Book(formBookName, formBookAuthor, formBookPages)
+
+  myLibrary.push(newBooks)
+  console.log(myLibrary)
+  renderBook()
+}
 
 function renderBook() {
-    let bookTemplate = ''
-    myLibrary.forEach((book) => {
-        bookTemplate += `
+  let bookTemplate = ''
+  myLibrary.forEach((book) => {
+    bookTemplate += `
         <div class="books" book-attribute="main-book">
             <p class="book_name">${book.bookName}</p>
             <p class="book_author">${book.author}</p>
             <p class="book_num-page">${book.numberOfPages}</p>
         </div>
         `
-    })
+  })
 
-    document.querySelector('.book-here').innerHTML = bookTemplate
+  document.querySelector('.book-here').innerHTML = bookTemplate
 }
 
 renderBook()
 
 function addBookToLibrary() {
-    let template = ''
-    for(let i = 0; i < myLibrary.length; i++) {
-        
-    }  
+  let template = ''
+  for (let i = 0; i < myLibrary.length; i++) {}
 }
 
 console.log()
--- bookData.js
+++ bookData.js
@@ -1,25 +1,24 @@
 function addFormDom() {
-    let formTemplate = `
+  let formTemplate = `
         <div class="form-overlay" overlay-attribute='form-attr'>
             <form class="book-form">
-                <input type="text" name="bookname" placeholder="Book Name">
-                <input type="text" name="bookauthor" placeholder="Author">
-                <input type="number" name="bookpages" placeholder="Number of Pages">
+                <input type="text" name="bookname" id="book-name" placeholder="Book Name">
+                <input type="text" name="bookauthor" id="book-author" placeholder="Author">
+                <input type="number" name="bookpages" id="book-pages" placeholder="Number of Pages">
+                <button type="submit" class="sub-btn" submitbook-attribute="submit-book">Submit</button>
             </form>
         </div>`
 
-        document.querySelector('.form-here').innerHTML = formTemplate
+  document.querySelector('.form-here').innerHTML = formTemplate
 }
 
-
 function addBookBtn(e) {
-    let addBtn = `
+  let addBtn = `
         <div class="btn-wrapper">
             <button class="add-btn">Add Book</button>
         </div>`
-    
 
-    document.querySelector('.btn-here').innerHTML = addBtn
+  document.querySelector('.btn-here').innerHTML = addBtn
 }
 
-export {addFormDom, addBookBtn}
+export { addFormDom, addBookBtn }
